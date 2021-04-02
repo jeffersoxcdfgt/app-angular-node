2567606e277b0ee9a8a778208fd2f23795341099
Jefferson Medina <jefferson.medina@sourcemeridian.com>
2021-04-01 23:20:49 -0500

    Sendata obsevable

--- front-app/src/app/private/login-admin/login-admin.component.ts
+++ front-app/src/app/private/login-admin/login-admin.component.ts
@@ -5,7 +5,7 @@ import { loginUser } from '../store/actions/login.actions';
 import { erroLogging } from '../store/reducers/login.reducers';
 import { User } from '../shared/user';
 import { ValidationLoginService } from '../../shared/validations/validationLogin';
-import { BehaviorSubject , of   } from 'rxjs';
+import { BehaviorSubject , combineLatest } from 'rxjs';
 
 class Error {
   messageError: string;
@@ -22,10 +22,19 @@ export class LoginAdminComponent implements OnInit {
   messageValidation = '';
   showAlert = 'none';
 
+  subgetEmail: BehaviorSubject<string> = new BehaviorSubject<string>('')
+  obsgetEmail: any;
+
+  subgetPassword: BehaviorSubject<string> = new BehaviorSubject<string>('')
+  obsgetPassword: any;
+
   constructor(
     private store: Store<AppState>,
     public validationLoginService: ValidationLoginService
-    ){}
+    ){
+        this.obsgetEmail = this.subgetEmail
+        this.obsgetPassword = this.subgetPassword       
+    }
 
     ngOnInit(): void{
       this.validationLoginService.initValidation();
@@ -34,12 +43,19 @@ export class LoginAdminComponent implements OnInit {
 
 
     login(): void {
-        if (this.validationLoginService.ifGood()){
-            const user: User = {
-              email: 'test@test.com',
-              password: '1234'
-            };
-            this.store.dispatch(loginUser({user}));
+
+      this.obsgetEmail = this.subgetEmail
+      this.obsgetPassword = this.subgetPassword  
+
+        if (this.validationLoginService.ifGood()){           
+            combineLatest([this.obsgetEmail , this.obsgetPassword])
+            .subscribe(([email,password])=>{
+                const user:User  = {
+                  email: email.toString(),
+                  password: password.toString()
+                };
+                this.store.dispatch(loginUser({user}));
+            })
         }
     }
 
@@ -47,8 +63,8 @@ export class LoginAdminComponent implements OnInit {
       this.store.select(erroLogging).subscribe((error) => {
         if (error !== null){
           const erromessage = Object.assign(new Error(), error);
-          this.messageValidation = erromessage.messageError;
-          this.showAlert = 'block';
+          this.messageValidation = erromessage.messageError; 
+         // this.showAlert = 'block';
         }
       });
     }
