93086fb68aabeadd3e4ae8a6251d5c76f98fd9c1
Jefferson Medina <jefferson.medina@sourcemeridian.com>
2021-06-12 15:33:56 -0500

    Update search for private products

--- front-app/src/app/private/product/product-list/product-list.component.ts
+++ front-app/src/app/private/product/product-list/product-list.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../../app.state';
 import { Product } from '../class/product';
-import { Observable } from 'rxjs';
+import { Observable , of, BehaviorSubject } from 'rxjs';
+import { mergeMap , switchMap , map } from 'rxjs/operators';
 import { getAllProducts } from '../store/reducers/product.reducers';
 
 @Component({
@@ -14,14 +15,34 @@ export class ProductListComponent implements OnInit {
 
   p: any;
   products: Observable<Product[]>;
+  auxproducts: Observable<Product[]>;
+  subgetSearch: BehaviorSubject<string> = new BehaviorSubject<string>('');
+
   constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
     this.products = this.store.select(getAllProducts);
+    this.auxproducts = this.products;
+    this.searchData();
     this.products.subscribe( data => {
       if (data.length > 0){
         // console.log(data,"private")
       }
     });
   }
+
+  searchData = () => {
+    const source =  this.subgetSearch
+      .pipe(mergeMap((inputdata: string) =>
+           this.products.pipe(
+                      map((allproducts: Product[]) => ( allproducts.filter((oneproduct) =>
+                        oneproduct.name.toLowerCase().includes(inputdata.toLowerCase()) === true))),
+                             switchMap(valpro => ( inputdata === '' ? this.auxproducts : of<Product[]>(valpro))),
+           )
+         ),
+       map((becomeproduct) =>  this.products = of<Product[]>(becomeproduct)),
+    );
+    source.subscribe((myproducts) => this.products = myproducts);
+}
+
 }
