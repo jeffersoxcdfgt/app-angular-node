f5dd2a310b37e8442f256aa58335d29b42fb8e2a
jefferson <jefferson.medina@sourcemeridian.com>
2021-09-14 11:11:53 -0500

    update feature for app

--- front-app/src/app/private/product/product-edit/product-edit.component.ts
+++ front-app/src/app/private/product/product-edit/product-edit.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { filter, find, map, mergeMap, tap } from 'rxjs/operators';
+import { AppState } from 'src/app/app.state';
+import { Product } from '../class/product';
+import { productGet } from '../store/actions/product.actions';
+import { getProduct } from '../store/reducers/product.reducers';
 
 @Component({
   selector: 'app-product-edit',
@@ -7,9 +15,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductEditComponent implements OnInit {
 
-  constructor() { }
+  product: Observable<Product>;
+
+    // inputs data
+
+   subgetname: BehaviorSubject<string> = new BehaviorSubject<string>('');
+   obsgetname: any;
+   nameproduct = '';
+
+
+    // inputs data
+
+  constructor(private route: ActivatedRoute ,
+              private store: Store<AppState>){
+    this.obsgetname = this.subgetname;
+  }
 
   ngOnInit(): void {
+
+    this.route.params.subscribe( params => {
+      this.store.dispatch(productGet({id: +params.id}));
+    });
+
+    this.product = this.store.select(getProduct).pipe(
+     map((dataVal) => dataVal != null && dataVal !== undefined ? dataVal[0] : dataVal),
+      find((val) => val !== null)
+    );
+
+    this.product.subscribe((pro) => {
+        this.nameproduct = pro.name;
+        this.subgetname.next(pro.name);
+    });
+
   }
 
 }
